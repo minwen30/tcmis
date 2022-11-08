25934b8c23a3d9256a7f7f18478f45a54975180c
minwen30 <s1100382@gm.pu.edu.tw>
2022-11-08 22:14:19 +0800

    make it better

--- index.py
+++ index.py
@@ -2,7 +2,6 @@ import firebase_admin
 from firebase_admin import credentials, firestore
 cred = credentials.Certificate("serviceAccountKey.json")
 firebase_admin.initialize_app(cred)
-db = firestore.client()
 
 from flask import Flask, render_template, request
 from datetime import datetime, timezone, timedelta
